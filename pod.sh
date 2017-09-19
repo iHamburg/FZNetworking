f199a7a14f816316a9890b9e78f468572834c7c5
Forest <yiqing.zhang@bl.com>
2017-09-19 17:42:38 +0800

    1

--- pod.sh
+++ pod.sh
@@ -1,14 +1,14 @@
 Project = 'FZNetworking'
-VersionString=`grep -E 's.version.*=' {Project}.podspec`
+VersionString=`grep -E 's.version.*=' ${Project}.podspec`
 VersionNumber=`tr -cd '0-9'<<<"$VersionString"`
 #echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 #
 NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' {Project}.podspec | cut -d : -f1`
+LineNumber=`grep -nE 's.version.*=' ${Project}.podspec | cut -d : -f1`
 
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" {Project}.podspec
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" ${Project}.podspec
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
 
-pod trunk push {Project}.podspec
+pod trunk push ${Project}.podspec
