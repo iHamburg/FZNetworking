e1eab6b6fa98cad90ab30389ee65e81680c228bd
iHamburg <tominfrankfurt@gmail.com>
2017-09-19 21:16:24 +0800

    update

--- FZNetworking.podspec
+++ FZNetworking.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'FZNetworking'
-  s.version          = '5'
+  s.version          = '5.1'
   s.summary          = 'A Network Util based on AFNetworking'
 
 # This description is used to generate tags and improve search results.
--- pod.sh
+++ pod.sh
@@ -1,13 +1,16 @@
-Project = echo 'FZNetworking'
+Project=FZNetworking
 VersionString=`grep -E 's.version.*=' ${Project}.podspec`
-VersionNumber=`tr -cd '0-9'<<<"$VersionString"`
-#echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+VersionNumber=`tr -cd '0-9.'<<<"$VersionString"`
+# 去除第一个。
+NewVersionNumber=${VersionNumber:1}
+#echo "current version is ${VersionNumber}, new version is ${Project}"
 #
-NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' ${Project}.podspec | cut -d : -f1`
+#NewVersionNumber=$(($VersionNumber + 1))
+#LineNumber=`grep -nE 's.version.*=' ${Project}.podspec | cut -d : -f1`
 
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" ${Project}.podspec
-git commit -am ${NewVersionNumber}
+#sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" ${Project}.podspec
+#git commit -am ${NewVersionNumber}
+
 git tag ${NewVersionNumber}
 git push origin master --tags
 
